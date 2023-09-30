d502ec9e8bdcad3dcb94624e6416dcd556785dcd
olexandra0pryshchepa <130248875+olexandra0pryshchepa@users.noreply.github.com>
2023-09-30 19:15:56 +0300

    render cocktails

--- src/js/render-card.js
+++ src/js/render-card.js
@@ -3,13 +3,17 @@ export function renderCard(arr, box) {
     .map(
       item =>
         `<li class="cocktail-card" data-id="${item._id}">
-      <img src="${item.drinkThumb}" alt"${item.drink}" width ="300"/>
+      <img class="cocktail-card-img" src="${item.drinkThumb}" alt="${item.drink}" width ="300"/>
           <h3 class="cocktail-title">${item.drink}</h3>
       <p class="cocktail-description">${item.description}</p>
        <div>
+        <div class="render-cards-buttons-wrapper">
        <button type="button" class="card-button" href="#">Learn More</button>
-       <button class="add-to-localstorage-btn" href="#">ADD</button>
+       <button class="add-to-localstorage-btn" href="#"><svg width="18px" height="18px" class="svg-icon-heart">
+              <use href="./img/sprite.svg#icon-heart"></use>
+            </svg></button>
        <button class="remove-from-localstorage-btn" href="#">REMOVE</button>
+       </div>
        </div></li>`
     )
     .join('');

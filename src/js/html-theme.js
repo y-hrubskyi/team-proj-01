3fa8693d97b8fb90ce2709637587a9d29caa7c19
Rhdshfeyhccfu <130246853+Mykola1612@users.noreply.github.com>
2023-10-01 03:04:03 +0300

    Bugfix/header

--- src/js/html-theme.js
+++ src/js/html-theme.js
@@ -3,28 +3,33 @@ const lightThemeEL = document.querySelector('.light-icon');
 const darkThemeEl = document.querySelector('.dark-icon');
 const bodyEl = document.body;
 
+let theme = bodyEl.className;
+
 const themeLocal = localStorage.getItem('theme');
 
-bodyEl.classList.add(`${themeLocal}`);
-
-let theme = bodyEl.className;
+if (themeLocal === 'dark-theme') {
+  bodyEl.classList.remove('light-theme');
+  bodyEl.classList.add(`${themeLocal}`);
+} else {
+  bodyEl.classList.add(`${themeLocal}`);
+}
 
 buttonThemeEl.addEventListener('click', onButtonThemeElClick);
 
 function onButtonThemeElClick() {
   if (bodyEl.classList.contains('light-theme') === true) {
     theme = 'dark-theme';
-    bodyEl.classList.add('dark-theme');
+
     bodyEl.classList.remove('light-theme');
-    lightThemeEL.classList.add('visually-hidden');
-    darkThemeEl.classList.remove('visually-hidden');
+    bodyEl.classList.add('dark-theme');
+
     localStorage.setItem('theme', theme);
   } else {
     theme = 'light-theme';
-    lightThemeEL.classList.remove('visually-hidden');
-    darkThemeEl.classList.add('visually-hidden');
+
     bodyEl.classList.add('light-theme');
     bodyEl.classList.remove('dark-theme');
+
     localStorage.setItem('theme', theme);
   }
 }

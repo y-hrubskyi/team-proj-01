641f09bb4700c2fa899e590c2679b3286dc7893d
PiganS <pigggggan@gmail.com>
2023-10-01 22:31:14 +0300

    setup-click-handler

--- src/js/setup-handlers.js
+++ src/js/setup-handlers.js
@@ -2,8 +2,9 @@ import { addToLocalStorage, removeFromLocalStorage } from './local-storage';
 
 export function setupClickHandlerOnAddToLS(data, box) {
   box.addEventListener('click', function (e) {
-    if (e.target.classList.contains('add-to-localstorage-btn')) {
-      const cardId = e.target.closest('.cocktail-card').dataset.id;
+    const button = e.target.closest('.add-to-localstorage-btn');
+    if (button) {
+      const cardId = button.closest('.cocktail-card').dataset.id;
       const selectedCard = data.find(item => item._id === cardId);
       if (selectedCard) {
         addToLocalStorage(selectedCard);

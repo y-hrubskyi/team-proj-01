cd95d5798b55fb77e7d024b32ef6dd3bf3c50fef
y-hrubskyi <y.hrubskyi@gmail.com>
2023-10-02 19:48:39 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import './js/random-cocktails';
 import './js/drop-down';
 import './js/drop-down-modal';
 import './js/html-theme';
-import './js/favorite-cocktails';
 import './js/coctails-search';
 import './js/page-scroll';
+// import './js/favorite-cocktails';

1da8efbbb134d2c05dd0354ff27f85474d916d05
y-hrubskyi <y.hrubskyi@gmail.com>
2023-10-03 21:45:05 +0300

    Update coctails-search.js
    update sprite-path

--- src/js/coctails-search.js
+++ src/js/coctails-search.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import spriteUrl from '/img/sprite.svg';
 
 import { BASE_URL, LOCAL_STORAGE_KEYS } from './constants';
 import { renderCocktails } from './render-functions';
@@ -22,56 +23,41 @@ export async function searchCocktailsByFillter({
   if (cocktailName) {
     requestURL.searchParams.append(`s`, cocktailName);
   }
-  // try {
-    const response = await axios.get(requestURL);
 
-    return response.data;
-  // }
-  //  catch (error){
-  //    renderNoResultInfo()
-  // }
+  const response = await axios.get(requestURL);
+  return response.data;
 }
 
 export async function renderSearchResults({ firstLetter, cocktailName } = {}) {
-   //add function render card with informaton of no resuts
-// if (!firstLetter && !cocktailName) {
-//     renderNoResultInfo()
-//     return
-//   };
-console.log(firstLetter, cocktailName);
   try {
     const searchResults = await searchCocktailsByFillter({
-    firstLetter,
-    cocktailName,
-  }); //add function render card with informaton of no resuts
+      firstLetter,
+      cocktailName,
+    }); //add function render card with informaton of no resuts
 
-  const cocktailsToRender =
-      getDeviceType() === 'desktop' ? 9 : 8;
+    const cocktailsToRender = getDeviceType() === 'desktop' ? 9 : 8;
     renderResultInfo();
-    const randomCocktailsList = document.querySelector('.random-cocktails-list-js');
+    const randomCocktailsList = document.querySelector(
+      '.random-cocktails-list-js'
+    );
     newTextAfterSearch();
-  // randomCocktailsList.innerHTML = '';
-  const paginationFn = paginateArray(
-    searchResults,
-    cocktailsToRender,
-    randomCocktailsList
-  );
+    const paginationFn = paginateArray(
+      searchResults,
+      cocktailsToRender,
+      randomCocktailsList
+    );
 
-  renderCocktails(paginationFn, randomCocktailsList);
-
-  // renderCocktails(cocktailsToRender, randomCocktailsList);
-  setupClickHandlerOnWorkWithLocaleStorage(
-    searchResults,
-    randomCocktailsList,
-    LOCAL_STORAGE_KEYS.COCKTAILS
-  );
-  setupClickHandlerOnOpenModal(randomCocktailsList);
+    renderCocktails(paginationFn, randomCocktailsList);
+    setupClickHandlerOnWorkWithLocaleStorage(
+      searchResults,
+      randomCocktailsList,
+      LOCAL_STORAGE_KEYS.COCKTAILS
+    );
+    setupClickHandlerOnOpenModal(randomCocktailsList);
   } catch {
-    
-    renderNoResultInfo()
+    renderNoResultInfo();
   }
- 
-};
+}
 
 // renderSearchResults();
 // search input //
@@ -106,19 +92,16 @@ function onRadioButtonClick(e) {
   renderSearchResults({ firstLetter: letter });
 }
 
-
-
 radionbuttonsEL.addEventListener('click', onRadioButtonClick);
-
 
 // no result image
 
 function renderNoResultInfo() {
-  const noResultsContainerEl = document.querySelector(".cocktails-section")
+  const noResultsContainerEl = document.querySelector('.cocktails-section');
 
-  noResultsContainerEl.innerHTML =  `<div class="placeholder-empty-favorite-list">
+  noResultsContainerEl.innerHTML = `<div class="placeholder-empty-favorite-list">
     <svg class="empty-favorite-img">
-      <use href="./img/sprite.svg#icon-placeholder-empty-list"></use>
+      <use href="${spriteUrl}#icon-placeholder-empty-list"></use>
     </svg>
     <p class="empty-favorite-text">
       Sorry, we
@@ -126,39 +109,22 @@ function renderNoResultInfo() {
       any cocktail for you
     </p>
   </div>`;
-
-  // noResultsContainerEl.insertAdjacentHTML("beforeend",`<div class="placeholder-empty-favorite-list">
-  //   <svg class="empty-favorite-img">
-  //     <use href="./img/sprite.svg#icon-placeholder-empty-list"></use>
-  //   </svg>
-  //   <p class="empty-favorite-text">
-  //     Sorry, we
-  //     <span class="empty-favorite-text-span">didn’t find</span>
-  //     any cocktail for you
-  //   </p>
-  // </div>`  );
-  
- 
-} 
-
+}
 
 // change coctails to Searching results
 
-
-
 function newTextAfterSearch() {
-const titleCocktailel = document.querySelector(".section-title");
-  titleCocktailel.textContent = "Searching results"
-  
+  const titleCocktailel = document.querySelector('.section-title');
+  titleCocktailel.textContent = 'Searching results';
 }
 
 function renderResultInfo() {
+  const resultsContainerEl = document.querySelector('.cocktails-section ');
 
-  const resultsContainerEl = document.querySelector(".cocktails-section ")
-
-  resultsContainerEl.innerHTML =   `<div class="container">
+  resultsContainerEl.innerHTML = `<div class="container">
     <h2 class="section-title">Cocktails</h2>
     <ul
       class="cocktails-list random-cocktails-list random-cocktails-list-js"
     ></ul>
-  </div>`};
+  </div>`;
+}

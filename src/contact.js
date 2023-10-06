fc6cdfc129b438a521c23ea2e8a296ab7d637641
y-hrubskyi <y.hrubskyi@gmail.com>
2023-10-06 18:44:02 +0300

    summary

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,5 @@
-import './js/html-theme';
-import './js/mobile-menu';
-import './js/drop-down';
-import './js/drop-down-modal';
-
+import './js/header/html-theme';
+import './js/header/mobile-menu';
+import './js/header/drop-down';
+import './js/header/drop-down-modal';
 import './js/swiper';
